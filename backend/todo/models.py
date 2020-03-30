3a7876602142a5aad23256cd9100fc05db9b24fe
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-03-30 22:12:42 +0000

    added admin

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Todo(models.Model):
     title=models.CharField(max_length=120)
-    description= models.textField()
-    completed+ models.BooleanField(default=False)
+    description= models.TextField()
+    completed=models.BooleanField(default=False)
 
 def _str_(self):
         return self.title
